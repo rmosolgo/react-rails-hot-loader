d4bcc302c03ca492439e92d8255a906287b6a140
Robert Mosolgo <rdmosolgo@gmail.com>
2015-06-17 13:29:46 -0700

    first

---
+++ lib/react/rails/hot/loader.rb
@@ -0,0 +1,11 @@
+require "react/rails/hot/loader/version"
+
+module React
+  module Rails
+    module Hot
+      module Loader
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/react/rails/hot/loader/version.rb
@@ -0,0 +1,9 @@
+module React
+  module Rails
+    module Hot
+      module Loader
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
